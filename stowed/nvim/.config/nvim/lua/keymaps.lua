6b450ef18d988ca11c4496400ba6ed7b87503308
Jonathan Fenstermacher <jfenstermacher123@gmail.com>
2021-09-06 21:45:53 -0400

    terminal binding included

--- stowed/nvim/.config/nvim/lua/keymaps.lua
+++ stowed/nvim/.config/nvim/lua/keymaps.lua
@@ -34,10 +34,10 @@ local mappings = {
 
   -- Window bindings
 
-  {'n', '<C-H>', ":lua win_move_or_split('h')<CR>", silent},
-  {'n', '<C-J>', ":lua win_move_or_split('j')<CR>", silent},
-  {'n', '<C-K>', ":lua win_move_or_split('k')<CR>", silent},
-  {'n', '<C-L>', ":lua win_move_or_split('l')<CR>", silent},
+  {'nt', '<C-H>', ":lua win_move_or_split('h')<CR>", silent},
+  {'nt', '<C-J>', ":lua win_move_or_split('j')<CR>", silent},
+  {'nt', '<C-K>', ":lua win_move_or_split('k')<CR>", silent},
+  {'nt', '<C-L>', ":lua win_move_or_split('l')<CR>", silent},
   {'n', '<leader>wH', '<c-w>5<'},
   {'n', '<leader>wJ', ':resize +5<cr>'},
   {'n', '<leader>wK', ':resize -5<cr>'},
