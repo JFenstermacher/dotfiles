90d1ed69287fe74f9afa3c337c3e0b9bf4bb03fe
Jonathan Fenstermacher <jfenstermacher123@gmail.com>
2021-12-26 17:33:42 -0500

    tweaks to stow script

--- stow.sh
+++ stow.sh
@@ -2,9 +2,20 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+STOW_ARGS="-t $HOME"
+ACTION="stow"
+
+# Unstow by passing -D as first arg
+if [[ "$1" == "-D" ]]; then
+  STOW_ARGS="$STOW_ARGS -D"
+  ACTION="unstow"
+  shift
+fi
+
+# Must specify at least one directory
 if [[ "$#" -eq 0 ]]; then
-  echo "[ERROR] Please specify one or more dotfiles to link"
-  echo "[ERROR] Specify 'all' as first arg to link everything"
+  echo "[ERROR] Please specify one or more dotfiles to $ACTION"
+  echo "[ERROR] Specify 'all' as first arg to $ACTION everything"
   exit 1
 fi
 
@@ -18,7 +29,7 @@ if [[ "$1" == "all" ]]; then
 else
   for dir in "$@"; do
     if [ ! -d "$SCRIPT_DIR/$dir" ]; then
-      echo "[WARN] $dir doesn't exist. Skipping."
+      echo "[WARN] '$dir' doesn't exist. Skipping."
     else
       TO_STOW+=($dir)
     fi
@@ -27,6 +38,6 @@ fi
 
 # Stow directory
 for dir in "${TO_STOW[@]}"; do
-  echo "[INFO] Stowing $dir"
-  stow -t $HOME $dir
+  echo "[INFO] Action: $ACTION; Directory: '$dir'"
+  stow $STOW_ARGS $dir
 done
