3bd0d2c4320e6f35196a56faf01199530a2895b8
Jonathan Fenstermacher <jfenstermacher123@gmail.com>
2022-01-19 20:52:22 -0500

    NVIM: not using vsnip, commenting out vsnip tab completion for cmp

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -12,8 +12,8 @@ end
 local tab_complete = function(fallback)
   if cmp.visible() then
     cmp.select_next_item()
-  elseif vim.fn["vsnip#available"](1) == 1 then
-    feedkey("<Plug>(vsnip-expand-or-jump)", "")
+  -- elseif vim.fn["vsnip#available"](1) == 1 then
+  --   feedkey("<Plug>(vsnip-expand-or-jump)", "")
   elseif has_words_before() then
     cmp.complete()
   else
@@ -24,8 +24,8 @@ end
 local shift_tab_complete = function()
   if cmp.visible() then
     cmp.select_prev_item()
-  elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-    feedkey("<Plug>(vsnip-jump-prev)", "")
+  -- elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+  --   feedkey("<Plug>(vsnip-jump-prev)", "")
   end
 end
 
