954705c84a9ea74766b1150142279b3661e2fdb2
Jonathan Fenstermacher <jfenstermacher123@gmail.com>
2022-04-25 11:05:22 -0400

    NVIM: comment fixes

--- nvim/.config/nvim/lua/pack.lua
+++ nvim/.config/nvim/lua/pack.lua
@@ -63,24 +63,24 @@ use{
     }
   },
 
-  -- -- Git
+  -- Git
   ['tpope/vim-fugitive'] = {
     cmd = { 'Git', 'Gdiff' },
   },
 
-  -- -- LSP
+  -- LSP
   ['neovim/nvim-lspconfig'] = {
     as = 'lsp',
     requires = {{'williamboman/nvim-lsp-installer'}}
   },
 
-  -- -- Treesitter
+  -- Treesitter
   ['nvim-treesitter/nvim-treesitter'] = {
     as = "treesitter",
     run = ":TSUpdate"
   },
 
-  -- -- Complete
+  -- Complete
   ['hrsh7th/nvim-cmp'] = {
     as = 'cmp',
     requires = {
@@ -94,16 +94,16 @@ use{
     }
   },
 
-  -- -- Comments
+  -- Comments
   ['tpope/vim-commentary'] = {},
 
-  -- -- Movements Improvement
+  -- Movements Improvement
   ['junegunn/vim-easy-align'] = {},
   ['justinmk/vim-sneak'] = {},
   ['tpope/vim-vinegar'] = {},
   ['tpope/vim-surround'] = {},
 
-  -- -- Database
+  -- Database
   ['kristijanhusak/vim-dadbod-ui'] = {
     as = "dadbod",
     cmd = { 'DBUIToggle', 'DBUIAddConnection', 'DBUI', 'DBUIFindBuffer', 'DBUIRenameBuffer' },
@@ -113,7 +113,7 @@ use{
     }
   },
 
-  -- -- Status Line
+  -- Status Line
   ['nvim-lualine/lualine.nvim'] = { as = 'lualine' },
 
   ['voldikss/vim-floaterm'] = { as = "floaterm" }
