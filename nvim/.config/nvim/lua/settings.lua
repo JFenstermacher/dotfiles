5c8cd1a549af9a5ae364c306ab58fd4ccd8e830e
Jonathan Fenstermacher <jfenstermacher123@gmail.com>
2022-04-27 10:42:33 -0400

    NVIM: updated settings

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -1,68 +1,53 @@
 local cmd = vim.cmd
-local o, wo, bo = vim.o, vim.wo, vim.bo
 local indent = 2
+local g = vim.g
 
-local global = {o}
-local window = {o, wo}
-local buffer = {o, bo}
-
-local M = {
-  global = global,
-  window = window,
-  buffer = buffer
-}
-
-function M.bind_opt(opt, value, scopes)
-  for _, scope in ipairs(scopes) do scope[opt] = value end
-end
-
-function M.bind_opts(opts)
-  for _, opt in ipairs(opts) do M.bind_opt(unpack(opt)) end
-end
+local M = {}
 
 function M.bind_globals(opts)
   for key, value in pairs(opts) do
-    vim.g[key] = value
+    g[key] = value
   end
 end
 
-local opts = {
-  -- Global Scoped Options
+function M.bind_opts(opts)
+  local set = vim.opt
+  for key, value in pairs(opts) do
+    set[key]  = value
+  end
+end
 
-  {'autowrite', true, global},
-  {'background', 'dark', global},
-  {'belloff', 'all', global},
-  {'clipboard', 'unnamed,unnamedplus', global},
-  {'completeopt', 'menu,menuone,noselect', global},
-  {'display', 'lastline', global},
-  {'guicursor', '', global},
-  {'hidden', true, global},
-  {'ignorecase', true, global},
-  {'scrolloff', 8, global},
-  {'showcmd', true, global},
-  {'smartcase', true, global},
-  {'splitbelow', true, global},
-  {'splitright', true, global},
-  {'termguicolors', true, global},
-  {'wildmode', 'list:longest', global},
-
-  -- Window Scoped Options
-
-  {'colorcolumn', '120', window},
-  {'wrap', true, window},
-  {'number', true, window},
-  {'relativenumber', true, window},
-
-  -- Buffer Scoped Options
-
-  {'autoindent', true, buffer},
-  {'expandtab', true, buffer},
-  {'shiftwidth', indent, buffer},
-  {'softtabstop', indent, buffer},
-  {'tabstop', indent, buffer}
+M.bind_opts{
+  autowrite      = true,                    -- Autowrite buffer when leaving buffer
+  background     = 'dark',                  -- Inherite background to use
+  belloff        = 'all',                   -- Please stop
+  clipboard      = 'unnamedplus',           -- Connection to system clipboard
+  completeopt    = {'menuone', 'noselect'}, -- Options for insert mode completion
+  hidden         = true,                    -- Ignore unsaved buffers
+  ignorecase     = true,                    -- Case insensitive searching
+  scrolloff      = 8,                       -- Number of lines to keep above and below the cursor
+  smartcase      = true,                    -- Case sensitivie searching
+  splitbelow     = true,                    -- Splitting a new window below the current one
+  splitright     = true,                    -- Splitting a new window at the right of the current one
+  termguicolors  = true,                    -- Enable 24-bit RGB color in the TUI
+  undofile       = true,                    -- Enable persistent undo
+  hlsearch       = true,                    -- Highlight all the matches of search pattern
+  swapfile       = false,                   -- Disable use of swapfile for the buffer
+  colorcolumn    = '120',                   -- Colored column at 120
+  wrap           = true,                    -- Disable wrapping of lines longer than the width of window
+  number         = true,                    -- Show numberline
+  relativenumber = true,                    -- Show relative numberline
+  expandtab      = true,                    -- Enable the use of space in tab
+  shiftwidth     = indent,                  -- Number of space inserted for indentation
+  tabstop        = indent,                  -- Number of space in a tab
+  fileencoding   = "utf-8",                 -- File content encoding for the buffer
+  spelllang      = "en",                    -- Support US english
 }
 
-M.bind_opts(opts)
+M.bind_globals{
+  do_filetype_lua = 1,    -- use filetype.lua
+  did_load_filetypes = 0  -- don't use filetype.vim
+}
 
 -- Highlight on yank
 cmd 'au TextYankPost * silent! lua vim.highlight.on_yank {on_visual=false}'
