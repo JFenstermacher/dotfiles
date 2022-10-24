7f73434528bbc634a61d6d18d2f48c9d51cf7261
Jonathan Fenstermacher <jfenstermacher123@gmail.com>
2022-10-23 23:40:02 -0400

    remove comment

--- nvim/.config/nvim/lua/core/pack.lua
+++ nvim/.config/nvim/lua/core/pack.lua
@@ -1,7 +1,3 @@
--- author: glepnr https://github.com/glepnir
--- date: 2022-07-02
--- License: MIT
-
 local uv, api = vim.loop, vim.api
 local helper = require('core.helper')
 local vim_path = helper.get_config_path()
