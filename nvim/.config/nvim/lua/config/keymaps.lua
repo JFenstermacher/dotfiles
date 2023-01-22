80426dfa5f3d42e5f8ee844833b25e5d88292562
Jonathan Fenstermacher <jfenstermacher123@gmail.com>
2023-01-22 18:04:51 -0500

    NVIM: add additional window split keybinds

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jk", "<esc>")
+
+-- Window binds
+vim.keymap.set("n", "<leader>wv", "<c-w>v", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>ws", "<c-w>s", { desc = "Split window horizontally" })
